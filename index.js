934f573fc7b9d89908ae50f2282bf189979cd196
Andre Violin <andreviolin@gmail.com>
2018-11-12 18:07:37 -0300

    ADD: Categorias

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
 const bodyParser = require('body-parser')
-app.set('view engine', 'ejs')
 
+app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded())
 
 const port = process.env.PORT || 3000
 
-let i = 0
-const ola = 'Olá Maycon'
-app.get('/', (request, response) => {
-    i++
-    response.render('index', { i, ola} )
+app.get('/', async (request, response) => {
+    const content = await axios.get('https://como-fazer-info6.firebaseio.com/teste.json')
+    
+    response.render('index', { i : content.data.categoria } )
+})
+
+app.get('/categorias/nova', (req, res) => {
+    res.render('categorias/nova')
+})
+
+app.post('/categorias/nova', async (req, res) => {
+    await axios.post('https://como-fazer-info6.firebaseio.com/categorias.json',{
+        categoria: req.body.categoria 
+    })
+    res.send(req.body)
+})
+
+app.get('/categorias', async (req, res) => {
+    const content = await axios.get('https://como-fazer-info6.firebaseio.com/categorias.json')
+    res.render('categorias/index', { categorias: content.data })
 })
 
 app.listen(port, (err) => {
@@ -21,3 +37,4 @@ app.listen(port, (err) => {
         console.log('Quase Twitter App está rodando na porta:', port)
     }
 })
+
